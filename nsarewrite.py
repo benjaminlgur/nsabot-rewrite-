f507eda59e5859a2d4bb15815c835f454710df59
benjaminlgur <benjaminlgur@yahoo.com>
2018-10-13 14:46:21 -0400

    Add files via upload

--- nsarewrite.py
+++ nsarewrite.py
@@ -1,4 +1,4 @@
-#NSAbot using discord.py rewrite by Grunt67
+#NSAbot using discord.py rewrite by Grunt67 made for python 3.7
 
 import discord
 import pymysql.cursors
@@ -29,16 +29,36 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    #code for role mentions.
     role_count = len(message.role_mentions)
     for x in range (0, role_count):
         game = message.role_mentions[x].name
-        user = message.author.name
+        user = message.author.display_name
         server = message.guild.name
+        channel = message.channel.name
         weekday = nsahelp.out_week_day()
         date = nsahelp.out_date()
         time = nsahelp.out_time()
-        sql = "INSERT INTO fsa.info(role, user, server, dayOfWeek, date, time) VALUES (%s, %s, %s, %s, %s, %s)"
-        val = (game, user, server, weekday, date, time)
+        sql = "INSERT INTO fsa.roleinfo(role, user, server, channel, dayOfWeek, date, time) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (game, user, server, channel, weekday, date, time)
+        try:
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print(mycursor.rowcount, "record inserted.")
+        except:
+            print("SQL server error")
+    #code for user mentions
+    member_count = len(message.mentions)
+    for x in range (0, member_count):
+        ping = message.mentions[x].display_name
+        user = message.author.display_name
+        server = message.guild.name
+        channel = message.channel.name
+        weekday = nsahelp.out_week_day()
+        date = nsahelp.out_date()
+        time = nsahelp.out_time()
+        sql = "INSERT INTO fsa.pinginfo(ping, user, server, channel, dayOfWeek, date, time) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (ping, user, server, channel, weekday, date, time)
         try:
             mycursor.execute(sql, val)
             mydb.commit()
